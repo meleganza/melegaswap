b5a0adb12329c336e3f3ba227c5f8e81467bf5c7
shani <loginshani@gmail.com>
2023-02-03 19:03:58 +0500

    farms

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -573,7 +573,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '0.001',
+    tokenPerBlock: '0.003',
     sortOrder: 1,
     isFinished: false,
   }
