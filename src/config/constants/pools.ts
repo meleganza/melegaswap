0ddf4bb8a224ee4b8447031d148361a5db68bea2
connectshani <loginshani@gmail.com>
2023-04-30 10:10:15 +0500

    update

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -498,7 +498,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '26',
+    tokenPerBlock: '185',
     sortOrder: 1,
     isFinished: false,
   }
