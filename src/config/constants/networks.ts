7bdd54a0c48dc6d07e9c3e8731a05e147fa69ba9
connectshani <loginshani@gmail.com>
2023-06-04 16:06:37 +0500

    update

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@pancakeswap/sdk'
 
 const NETWORK_URLS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: 'https://bsc-dataseed1.defibit.io',
+  [ChainId.MAINNET]: 'https://hardworking-quick-pine.bsc.discover.quiknode.pro/88bb8b036ef3ae642ee0af4e32e7886b91628cbf',
   [ChainId.TESTNET]: 'https://data-seed-prebsc-1-s1.binance.org:8545',
 }
 
