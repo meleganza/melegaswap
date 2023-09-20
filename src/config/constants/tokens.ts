2a88c562638fadfc9bbeb95b9cad5e60b0bcb3e0
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2023-09-20 16:34:19 +0200

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -3249,7 +3249,7 @@ const tokens = {
     decimals: 9,
     projectLink: 'https://www.Shibafomi.com',
   },
-  mx: {
+  mix: {
     symbol: 'MX',
     address: {
       56: '0xEF9d8E6918e4dE524cb768057Cd44374e449Aa8E',
