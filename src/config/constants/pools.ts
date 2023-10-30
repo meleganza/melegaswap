0b67edd8b1573e8454478322fc40e686beb0c367
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2023-10-30 05:26:43 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2148,7 +2148,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '6.94',
+    tokenPerBlock: '20',
     sortOrder: 1,
     isFinished: false,
   }
