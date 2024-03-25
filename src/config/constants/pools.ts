5481116728db76dc0f6923774eacf2d1c3a633b0
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2024-03-25 18:47:48 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2962,6 +2962,21 @@ const pools: PoolConfig[] = [
     sortOrder: 1,
     isFinished: false,
   }
+  ,
+  {
+    sousId: 199,
+    stakingToken: tokens.cake,
+    earningToken: tokens.msoc,
+    contractAddress: {
+      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
+      56: '0xB8e35550deE2BfFE861E0441831fd07271DA5082',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '1',
+    sortOrder: 1,
+    isFinished: false,
+  }
 ]
 
 export default pools
